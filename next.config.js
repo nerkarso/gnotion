b9dd3a3180e627245542e44f6acf931df34ccff8
Ner Karso <nerpkarso@gmail.com>
2024-11-15 22:11:35 -0300

    chore: turn off image optimization

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: '*',
-      },
-    ],
+    remotePatterns: [{ hostname: '*' }],
+    unoptimized: true,
   },
   reactStrictMode: true,
 };
