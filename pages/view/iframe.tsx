cc4cdd284dd5d324e8bc06043448600da0e6a8fb
Ner Karso <nerpkarso@gmail.com>
2025-12-27 00:28:01 -0300

    fix: overflow on iframe page

--- pages/view/iframe.tsx
+++ pages/view/iframe.tsx
@@ -1,5 +1,14 @@
+import { useEffect } from 'react';
+
 export default function Iframe({ pageId }: { pageId: string }) {
   const url = `${process.env.NEXT_PUBLIC_NOTION_PUBLISH_DOMAIN}/ebd/${pageId}`;
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
 
   return (
     <div className="iframe-root h-full">
